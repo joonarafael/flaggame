c1147e97bd8289adc83258b8227dec2520378c53
joonarafael <joona.kettunen21@gmail.com>
2024-04-15 11:51:53 +0300

    small small thing

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ echo "Initializing program environment and installing dependencies..."
 sudo apt-get update
 sudo apt-get install python3.6 python3-pip
 sudo apt-get install python3-tk
-pip install --upgrade pip
+python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade Pillow
 python3 -m pip install pytest coverage pylint autopep8 2to3
 chmod u+x ./run.sh ./test.sh ./pylint.sh ./coverage.sh
